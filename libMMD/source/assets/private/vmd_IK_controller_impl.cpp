42dc4394e62dbedbcfc8cae5b37ad7ec8d62f369
Aimidi <ai.mi.di@qq.com>
2023-07-10 00:52:52 +0800

    fix: vmd_IK_controller_impl compilation error

--- libMMD/source/assets/private/vmd_IK_controller_impl.cpp
+++ libMMD/source/assets/private/vmd_IK_controller_impl.cpp
@@ -12,43 +12,43 @@ Description:	MMD style IK enable information animation
 
 namespace libmmd
 {
-	std::string vmd_IK_controller::get_bone_name() const
+	std::string vmd_IK_controller_impl::get_bone_name() const
 	{
-		return std::string{bone_name_.begin(), bone_name_.end()};
+		return std::string{IK_bone_name_.begin(), IK_bone_name_.end()};
 	}
 
-	void vmd_IK_controller::set_bone_name(const std::string& name)
+	void vmd_IK_controller_impl::set_bone_name(const std::string& name)
 	{
-		bone_name_ = std::u8string{ name.begin(), name.end() };
+		IK_bone_name_ = std::u8string{ name.begin(), name.end() };
 	}
 
-	bool vmd_IK_controller::is_IK_enable() const
+	bool vmd_IK_controller_impl::is_IK_enable() const
 	{
-		return enable_;
+		return IK_enable_;
 	}
 
-	void vmd_IK_controller::set_IK_enable(bool value)
+	void vmd_IK_controller_impl::set_IK_enable(bool value)
 	{
-		enable_ = value;
+		IK_enable_ = value;
 	}
 
-	bool vmd_IK_controller::read_from_file(const file& file)
+	bool vmd_IK_controller_impl::read_from_file(const file& file)
 	{
 		Char ik_name[20]{ '\0' };
 		if (!file.read_elements(ik_name, 20LLU))
 			return false;
-		if (!file.read_elements(enable_))
+		if (!file.read_elements(IK_enable_))
 			return false;
-		bone_name_ = code_converter::shift_jis_to_utf8(ik_name);
+		IK_bone_name_ = code_converter::shift_jis_to_utf8(ik_name);
 		return true;
 	}
 
-	bool vmd_IK_controller::write_to_file(const file& file) const
+	bool vmd_IK_controller_impl::write_to_file(const file& file) const
 	{
-		const std::string name = code_converter::utf8_to_shift_jis(bone_name_);
+		const std::string name = code_converter::utf8_to_shift_jis(IK_bone_name_);
 		if (!file.write_elements(name.data(), 20LLU))
 			return false;
-		if (!file.write_elements(enable_))
+		if (!file.write_elements(IK_enable_))
 			return false;
 		return true;
 	}
