4b28c5c45ee9cc0eb7eb182dff547a0dcbbea154
AiMiDi <76622021+AiMiDi@users.noreply.github.com>
2023-07-19 16:18:32 +0800

    use libicu (#2)
    * feat: add libicu
    
    * feat: replace libiconv with libicu for encoding conversion shift-jis
    
    * feat: replace libiconv with libicu for encoding conversion utf16le
    
    * Removed submodule libMMD/dependency/libiconv

--- libMMD/source/core/private/libmmd_code_conversion.cpp
+++ libMMD/source/core/private/libmmd_code_conversion.cpp
@@ -9,55 +9,94 @@ Description:	Character coding conversion util
 **************************************************************************/
 
 #include "libmmd_code_conversion.h"
-#include "libiconv/iconv.h"
+#include "unicode/ucnv.h"
 
 namespace libmmd
 {
-	class iconv_converter
+	class ucnv_converter
 	{
-		libiconv_t conv_;
-	public:
-		iconv_converter(const char* to_code, const char* from_code) :
-			conv_(libiconv_open(to_code, from_code)) {}
-		~iconv_converter()
-		{
-			libiconv_close(conv_);
-		}
-		iconv_converter(const iconv_converter&) = delete;
-		void operator =(const iconv_converter&) = delete;
-		iconv_converter(iconv_converter&&) = delete;
-		void operator=(iconv_converter&&) = delete;
+		UErrorCode status = U_ZERO_ERROR;
 
-		size_t convert(const char** in_buf, size_t* in_bytes_left, char** out_buf, size_t* out_bytes_left) const
+		UConverter* ucnv_to;
+		UConverter* ucnv_from;
+
+	public:
+		ucnv_converter(const ::UChar* to_code, const ::UChar* from_code) :
+			ucnv_to(ucnv_openU(to_code, &status)),
+			ucnv_from(ucnv_openU(from_code, &status)) {}
+		~ucnv_converter()
 		{
-			return libiconv(conv_, in_buf, in_bytes_left, out_buf, out_bytes_left);
+			ucnv_close(ucnv_to);
+			ucnv_close(ucnv_from);
+		}
+		ucnv_converter(const ucnv_converter&) = delete;
+		void operator =(const ucnv_converter&) = delete;
+		ucnv_converter(ucnv_converter&&) = delete;
+		void operator=(ucnv_converter&&) = delete;
+
+		size_t convert(const char* source, const size_t source_length, char* target, const size_t target_capacity)
+		{
+			auto source_ptr = source;
+			const auto source_end = source + source_length;
+			auto target_ptr = target;
+			const auto target_end = reinterpret_cast<const char*>(target + target_capacity);
+			
+			ucnv_convertEx(ucnv_to, ucnv_from, &target_ptr, target_end, &source_ptr, source_end, nullptr, nullptr, nullptr, nullptr, true, true, &status);
+			return static_cast<size_t>(target_ptr - target);
+		}
+
+		[[nodiscard]] bool is_error() const
+		{
+			return !U_SUCCESS(status);
+		}
+
+		[[nodiscard]] bool is_init_error() const
+		{
+			return status == U_MEMORY_ALLOCATION_ERROR || status == U_FILE_ACCESS_ERROR;
+		}
+
+		[[nodiscard]] bool is_buffer_overflow() const
+		{
+			return status == U_BUFFER_OVERFLOW_ERROR;
 		}
 	};
 
 	std::string code_converter::utf8_to_shift_jis(const std::u8string& input_string)
 	{
 		size_t input_length = input_string.length();
-		std::string output_string(input_length, '\0');
-		const iconv_converter shift_jis_to_u8_converter("SHIFT_JIS", "UTF-8");
-		std::function<void(const iconv_converter&)> do_convert = 
-			[&output_string, &input_string, &input_length, &do_convert](const iconv_converter& converter)
+		if (input_length == 0)
 		{
-			size_t output_length = output_string.size();
+			return {};
+		}
+		std::string output_string(input_length, '\0');
+		ucnv_converter shift_jis_to_u8_converter(u"SHIFT_JIS", u"UTF8");
+		if (shift_jis_to_u8_converter.is_init_error())
+		{
+			ELOGE << "code_converter::utf8_to_shift_jis(): init converter error!";
+			return {};
+		}
+		std::function<void(ucnv_converter&)> do_convert = 
+			[&output_string, &input_string, &input_length, &do_convert](ucnv_converter& converter)
+		{
+			const size_t output_length = output_string.size();
 			char* output_buffer = output_string.data();
-			auto input_buffer = reinterpret_cast<const char*>(input_string.data());
-			if (converter.convert(&input_buffer, &input_length, &output_buffer, &output_length) == static_cast<size_t>(-1))
+			const auto input_buffer = reinterpret_cast<const char*>(input_string.data());
+			const size_t convertde_length = converter.convert(input_buffer, input_length, output_buffer, output_length);
+			if (converter.is_buffer_overflow())
 			{
-				if (errno == E2BIG)
-				{
-					output_string.resize(output_length * 2, '\0');
-					// retry
-					do_convert(converter);
-				}
+				// add buffer and retry.
+				output_string.resize(output_length * 2, '\0');
+				do_convert(converter);
 				return;
 			}
-			output_string.resize(output_length, '\0');
+			output_string.resize(convertde_length, '\0');
 		};
 		do_convert(shift_jis_to_u8_converter);
+		if (shift_jis_to_u8_converter.is_error())
+		{
+			ELOGE << "code_converter::utf8_to_shift_jis(): convert fail!";
+			return {};
+		}
 		return output_string;
 	}
 
@@ -65,175 +104,121 @@ namespace libmmd
 	{
 		std::u8string output_string{};
 		size_t input_length = input_string.length();
-		std::vector buffer_vector(input_length, '\0');
-		const iconv_converter u8_to_shift_jis_converter("UTF-8", "SHIFT_JIS");
-		std::function<void(const iconv_converter&)> do_convert = 
-			[&buffer_vector, &input_string, &input_length, &output_string, &do_convert](const iconv_converter& converter)
+		if (input_length == 0)
 		{
-			size_t buffer_length = buffer_vector.size();
-			char* output_men = buffer_vector.data();
-			char* output_buffer = output_men;
+			return output_string;
+		}
+
+		std::vector buffer_vector(input_length, '\0');
+		ucnv_converter u8_to_shift_jis_converter(u"UTF8", u"SHIFT_JIS");
+		if (u8_to_shift_jis_converter.is_init_error())
+		{
+			ELOGE << "code_converter::u8_to_shift_jis_converter(): init converter error!";
+			return {};
+		}
+		std::function<void(ucnv_converter&)> do_convert = 
+			[&buffer_vector, &input_string, &input_length, &output_string, &do_convert](ucnv_converter& converter)
+		{
+			const size_t buffer_length = buffer_vector.size();
+			char* output_buffer = buffer_vector.data();
 			const char* input_buffer = input_string.data();
-			if (converter.convert(&input_buffer, &input_length, &output_buffer, &buffer_length) == static_cast<size_t>(-1))
+			const size_t convertde_length = converter.convert(input_buffer, input_length, output_buffer, buffer_length);
+			if (converter.is_buffer_overflow())
 			{
-				if (errno == E2BIG)
-				{
-					buffer_vector.resize(buffer_length * 2, '\0');
-					// retry
-					do_convert(converter);
-				}
+				// add buffer and retry.
+				buffer_vector.resize(buffer_length * 2, '\0');
+				do_convert(converter);
 				return;
 			}
-			const size_t convected_size = buffer_vector.size() - buffer_length;
-			output_string.assign(reinterpret_cast<const char8_t*>(output_men), convected_size);
+			output_string.assign(reinterpret_cast<const char8_t*>(output_buffer), convertde_length);
 		};
 		do_convert(u8_to_shift_jis_converter);
+		if (u8_to_shift_jis_converter.is_error())
+		{
+			ELOGE << "code_converter::shift_jis_to_utf8(): convert fail!";
+			return {};
+		}
 		return output_string;
 	}
 
 	std::u16string code_converter::utf8_to_utf16_le(const std::u8string& input_string)
 	{
 		std::u16string output_string{};
-		std::u8string::size_type input_length = input_string.length();
-
+		size_t input_length = input_string.length();
 		if (input_length == 0)
 		{
 			return output_string;
 		}
 
-		auto input_string_ptr = reinterpret_cast<const unsigned char*>(input_string.data());
-		// 判断是否具有BOM(判断长度小于3字节的情况)
-		if (input_length > 3 && input_string_ptr[0] == 0xEF && input_string_ptr[1] == 0xBB && input_string_ptr[2] == 0xBF) 
+		std::vector buffer_vector(input_length, '\0');
+		ucnv_converter u8_to_u16le_converter(u"UTF16LE", u"UTF8");
+		if (u8_to_u16le_converter.is_init_error())
 		{
-			input_string_ptr += 3;
-			input_length -= 3;
+			ELOGE << "code_converter::utf8_to_utf16_le(): init converter error!";
+			return {};
 		}
-
-		// 开始转换
-		for (std::u8string::size_type i = 0; i < input_length; ++i) 
+		std::function<void(ucnv_converter&)> do_convert =
+			[&buffer_vector, &input_string, &input_length, &output_string, &do_convert](ucnv_converter& converter)
 		{
-			const uint32_t char1 = input_string_ptr[i];	// 取出UTF8序列首字节
-			if ((char1 & 0x80) == 0) {
-				// 最高位为0，只有1字节表示UNICODE代码点
-				output_string.push_back(static_cast<char16_t>(char1));
-				continue;
-			}
-			switch (char1 & 0xF0)
+			const size_t buffer_length = buffer_vector.size();
+			char* output_buffer = buffer_vector.data();
+			const auto input_buffer = reinterpret_cast<const char*>(input_string.data());
+			const size_t convertde_length = converter.convert(input_buffer, input_length, output_buffer, buffer_length);
+			if (converter.is_buffer_overflow())
 			{
-			case 0xF0: // 4 字节字符, 0x10000 到 0x10FFFF
-				{
-					const uint32_t char2 = input_string_ptr[++i];
-					const uint32_t char3 = input_string_ptr[++i];
-					const uint32_t char4 = input_string_ptr[++i];
-					// 计算UNICODE代码点值(第一个字节取低3bit，其余取6bit)
-					if (uint32_t code_point = (char1 & 0x07U) << 18 | (char2 & 0x3FU) << 12 | (char3 & 0x3FU) << 6 | (char4 & 0x3FU); code_point >= 0x10000)
-					{
-						// 在UTF-16中 U+10000 到 U+10FFFF 用两个16bit单元表示, 代理项对.
-						// 1、将代码点减去0x10000(得到长度为20bit的值)
-						// 2、high 代理项 是将那20bit中的高10bit加上0xD800(110110 00 00000000)
-						// 3、low  代理项 是将那20bit中的低10bit加上0xDC00(110111 00 00000000)
-						code_point -= 0x10000;
-						output_string.push_back(static_cast<char16_t>(code_point >> 10 | 0xD800U));
-						output_string.push_back(static_cast<char16_t>((code_point & 0x03FFU) | 0xDC00U));
-					}
-					else
-					{
-						// 在UTF-16中 U+0000 到 U+D7FF 以及 U+E000 到 U+FFFF 与Unicode代码点值相同.
-						// U+D800 到 U+DFFF 是无效字符, 为了简单起见，这里假设它不存在(如果有则不编码)
-						output_string.push_back(static_cast<char16_t>(code_point));
-					}
-				}
-				break;
-			case 0xE0: // 3 字节字符, 0x800 到 0xFFFF
-				{
-					const uint32_t char2 = input_string_ptr[++i];
-					const uint32_t char3 = input_string_ptr[++i];
-					// 计算UNICODE代码点值(第一个字节取低4bit，其余取6bit)
-					const uint32_t code_point = (char1 & 0x0FU) << 12 | (char2 & 0x3FU) << 6 | (char3 & 0x3FU);
-					output_string.push_back(static_cast<char16_t>(code_point));
-				}
-				break;
-			case 0xD0: // 2 字节字符, 0x80 到 0x7FF
-			case 0xC0:
-				{
-					const uint32_t char2 = input_string_ptr[++i];
-					// 计算UNICODE代码点值(第一个字节取低5bit，其余取6bit)
-					const uint32_t code_point = (char1 & 0x1FU) << 12 | (char2 & 0x3FU) << 6;
-					output_string.push_back(static_cast<char16_t>(code_point));
-				}
-				break;
-			default:	// 单字节部分(前面已经处理，所以不应该进来)
-				break;
+				// add buffer and retry.
+				buffer_vector.resize(buffer_length * 2, '\0');
+				do_convert(converter);
+				return;
 			}
+			output_string.assign(reinterpret_cast<const char16_t*>(output_buffer), convertde_length);
+		};
+		do_convert(u8_to_u16le_converter);
+		if (u8_to_u16le_converter.is_error())
+		{
+			ELOGE << "code_converter::utf8_to_utf16_le(): convert fail!";
+			return {};
 		}
-
 		return output_string;
 	}
 
 	std::u8string code_converter::utf16_le_to_utf8(const std::u16string& input_string)
 	{
 		std::u8string output_string{};
-		std::u16string::size_type input_length = input_string.length();
-
+		size_t input_length = input_string.length();
 		if (input_length == 0)
 		{
 			return output_string;
 		}
 
-		const char16_t* input_string_ptr = input_string.data();
-		if (input_string_ptr[0] == 0xFEFF) 
+		std::vector buffer_vector(input_length, '\0');
+		ucnv_converter u16le_to_u8_converter(u"UTF8", u"UTF16LE");
+		if (u16le_to_u8_converter.is_init_error())
 		{
-			input_string_ptr += 1;	//带有bom标记，后移
-			input_length -= 1;
+			ELOGE << "code_converter::utf16_le_to_utf8(): init converter error!";
+			return {};
 		}
-
-		// 开始转换
-		output_string.reserve(input_length * 3);
-		for (std::u16string::size_type i = 0; i < input_length; ++i) 
+		std::function<void(ucnv_converter&)> do_convert =
+			[&buffer_vector, &input_string, &input_length, &output_string, &do_convert](ucnv_converter& converter)
 		{
-			// 这里假设是在小端序下(大端序不适用)
-
-			// 1字节表示部分
-			if (const char16_t u16_char = input_string_ptr[i]; u16_char < 0x0080) 
+			const size_t buffer_length = buffer_vector.size();
+			char* output_buffer = buffer_vector.data();
+			const auto input_buffer = reinterpret_cast<const char*>(input_string.data());
+			const size_t convertde_length = converter.convert(input_buffer, input_length, output_buffer, buffer_length);
+			if (converter.is_buffer_overflow())
 			{
-				// u16char <= 0x007f
-				// U- 0000 0000 ~ 0000 07ff : 0xxx xxxx
-				output_string.push_back(static_cast<char8_t>(u16_char & 0x00FF));	// 取低8bit
+				// add buffer and retry.
+				buffer_vector.resize(buffer_length * 2, '\0');
+				do_convert(converter);
+				return;
 			}
-			// 2 字节能表示部分
-			else if (u16_char >= 0x0080 && u16_char <= 0x07FF) {
-				// * U-00000080 - U-000007FF:  110xxxxx 10xxxxxx
-				output_string.push_back(static_cast<char8_t>(((u16_char >> 6) & 0x1F) | 0xC0));
-				output_string.push_back(static_cast<char8_t>((u16_char & 0x3F) | 0x80));
-			}
-			// 代理项对部分(4字节表示)
-			else if (u16_char >= 0xD800 && u16_char <= 0xDBFF) {
-				// * U-00010000 - U-001FFFFF: 1111 0xxx 10xxxxxx 10xxxxxx 10xxxxxx
-				const uint32_t high_sur = u16_char;
-				const uint32_t low_sur = input_string_ptr[++i];
-				// 从代理项对到UNICODE代码点转换
-				// 1、从高代理项减去0xD800，获取有效10bit
-				// 2、从低代理项减去0xDC00，获取有效10bit
-				// 3、加上0x10000，获取UNICODE代码点值
-				uint32_t code_point = high_sur - 0xD800;
-				code_point <<= 10;
-				code_point |= low_sur - 0xDC00;
-				code_point += 0x10000;
-				// 转为4字节UTF8编码表示
-				output_string.push_back(static_cast<char8_t>(code_point >> 18 | 0xF0));
-				output_string.push_back(static_cast<char8_t>(((code_point >> 12) & 0x3F) | 0x80));
-				output_string.push_back(static_cast<char8_t>(((code_point >> 06) & 0x3F) | 0x80));
-				output_string.push_back(static_cast<char8_t>((code_point & 0x3F) | 0x80));
-				continue;
-			}
-			// 3 字节表示部分
-			else
-			{
-				// * U-0000E000 - U-0000FFFF:  1110xxxx 10xxxxxx 10xxxxxx
-				output_string.push_back(static_cast<char8_t>(((u16_char >> 12) & 0x0F) | 0xE0));
-				output_string.push_back(static_cast<char8_t>(((u16_char >> 6) & 0x3F) | 0x80));
-				output_string.push_back(static_cast<char8_t>((u16_char & 0x3F) | 0x80));
-			}
+			output_string.assign(reinterpret_cast<const char8_t*>(output_buffer), convertde_length);
+		};
+		do_convert(u16le_to_u8_converter);
+		if (u16le_to_u8_converter.is_error())
+		{
+			ELOGE << "code_converter::utf16_le_to_utf8(): convert fail!";
+			return {};
 		}
 		return output_string;
 	}
